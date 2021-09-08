4d7e3d0ea98f70d5c8fbbd3955b7b0af9ca58a8a
yukkiri <disbalance.nya@gmail.com>
2021-09-08 16:46:37 +0300

    sqlite auth

--- server/src/main/java/server/BDSQLite.java
+++ server/src/main/java/server/BDSQLite.java
@@ -1,5 +1,7 @@
 package server;
 
+import org.omg.PortableInterceptor.ServerRequestInfo;
+
 import java.sql.*;
 
 public class BDSQLite {
@@ -13,24 +15,34 @@ public class BDSQLite {
         connection = DriverManager.getConnection("jdbc:sqlite:CHAT.users");
     }
 
-    public static void onCreate() throws SQLException {
+    public static void onCreate() throws SQLException, ClassNotFoundException {
+        setConnection();
         statement = connection.createStatement();
         statement.execute("CREATE TABLE if NOT EXISTS 'users' " +
                 "('login' text PRIMARY KEY, 'password' text, 'nickname' text);");
     }
 
-    public static void newUser(String login, String password, String nickname) throws SQLException {
-        statement.execute("INSERT INTO 'users' ('login', 'password', 'nickname') VALUES ('" + login + "', '" + password + "', '" + nickname + "')");
+    public static boolean newUser(String log, String pass, String nick) throws SQLException {
+        resultSet = statement.executeQuery("SELECT * FROM 'users' WHERE 'login' = " + log + "OR 'nickname' = " + nick);
+        if (resultSet.wasNull()){
+            statement.execute("INSERT INTO 'users' ('login', 'password', 'nickname') VALUES ('" + log + "', '" + pass + "', '" + nick + "')");
+            return true;
+        }
+        return false;
     }
 
-    public static void loginUser() throws SQLException {
+    public static String loginUser(String log, String pass) throws SQLException {
         resultSet = statement.executeQuery("SELECT * FROM 'users'");
 
         while (resultSet.next()){
             String login = resultSet.getString("login");
             String password = resultSet.getString("password");
             String nickname = resultSet.getString("nickname");
+            if (log.equals(login) && pass.equals(password)){
+                return nickname;
+            }
         }
+        return null;
     }
 
     public static void closeConnection() throws SQLException {
--- server/src/main/java/server/SimpleAuth.java
+++ server/src/main/java/server/SimpleAuth.java
@@ -1,54 +1,78 @@
 package server;
 
 import java.io.DataOutputStream;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SimpleAuth implements Authentication{
 
+    private BDSQLite base = new BDSQLite();
+
     private class DataUser{
         String log;
         String pass;
         String nick;
-        public DataUser(String log, String pass, String nick){
-            this.log = log;
-            this.pass = pass;
-            this.nick = nick;
-        }
+
+
+//        public DataUser(String log, String pass, String nick){
+//            this.log = log;
+//            this.pass = pass;
+//            this.nick = nick;
+//        }
     }
 
     private List<DataUser> users;
 
     public SimpleAuth(){
-        users = new ArrayList<>();
-
-        users.add(new DataUser("qwe", "qwe", "qwe"));
-        users.add(new DataUser("asd", "asd", "asd"));
-        users.add(new DataUser("zxc", "zxc", "zxc"));
-
-        for (int i = 0; i < 10; i++) {
-            users.add(new DataUser("log" + i, "pass" + i, "nick" + i));
+        try {
+            base.onCreate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
+
+//        users = new ArrayList<>();
+//
+//        users.add(new DataUser("qwe", "qwe", "qwe"));
+//        users.add(new DataUser("asd", "asd", "asd"));
+//        users.add(new DataUser("zxc", "zxc", "zxc"));
+//
+//        for (int i = 0; i < 10; i++) {
+//            users.add(new DataUser("log" + i, "pass" + i, "nick" + i));
+//        }
     }
 
     @Override
     public String getNick(String log, String pass) {
-        for (DataUser user : users) {
-            if (user.log.equals(log) && user.pass.equals(pass)){
-                return user.nick;
-            }
+        try {
+            return base.loginUser(log, pass);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
         }
-        return null;
+//        for (DataUser user : users) {
+//            if (user.log.equals(log) && user.pass.equals(pass)){
+//                return user.nick;
+//            }
+//        }
     }
 
     @Override
     public boolean registration(String log, String pass, String nick) {
-        for (DataUser user : users) {
-            if (user.log.equals(log) || user.nick.equals(nick)){
-                return false;
-            }
+        try {
+            return base.newUser(log, pass, nick);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
         }
-        users.add(new DataUser(log, pass, nick));
-        return true;
+//        for (DataUser user : users) {
+//            if (user.log.equals(log) || user.nick.equals(nick)){
+//                return false;
+//            }
+//        }
+//        users.add(new DataUser(log, pass, nick));
+//        return true;
     }
 }
